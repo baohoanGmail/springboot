fcfd9c21cbb725a36469c3a8ea8138d8b87d71a3
baohoan90 <30434634+baohoan90@users.noreply.github.com>
2019-03-18 17:26:09 +0700

    Update ExampleController.java

--- src/main/java/com/hoan/lam/demo/ExampleController.java
+++ src/main/java/com/hoan/lam/demo/ExampleController.java
@@ -1,31 +1,64 @@
 package com.hoan.lam.demo;
 
-import org.springframework.web.bind.annotation.RestController;
-
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @RestController
 public class ExampleController {
 
-	@RequestMapping("/")
-	public String index() {
-		log.info("get method is calling... {}");
-		return "Greetings from Spring Boot!";
-	}
+  private static final ObjectMapper jsonMapper = new ObjectMapper();
+  static {
+    jsonMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    jsonMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
+    jsonMapper.setSerializationInclusion(Include.NON_NULL);
+  }
+  //
+  // private static <T> List<T> json2Object(Object values) {
+  // try {
+  // String jsonString = object2Json(values);
+  // return jsonMapper.readValue(jsonString, new TypeReference<List<T>>() {});
+  // } catch (IOException e) {
+  // log.error(e.getMessage());
+  // return Collections.emptyList();
+  // }
+  // }
 
-	@PostMapping("/load")
-	public String load(@RequestBody Object obj) {
-		if (obj != null) {
-			log.info("post method is loading... {}", obj.toString());
-			return obj.toString();
-		}
-		log.info("post method is loading...");
-		return "Greetings from Spring Boot!";
-	}
+  private static final String object2Json(Object values) {
+    try {
+      return jsonMapper.writeValueAsString(values);
+    } catch (JsonProcessingException e) {
+      log.error(e.getMessage());
+      return e.getMessage();
+    }
+  }
+
+  @RequestMapping("/")
+  public String index() {
+    log.info("get method is calling... {}");
+    return "Greetings from Spring Boot!";
+  }
+
+  @PostMapping("/load")
+  public String load(@RequestBody List<Bounce> requestBody) {
+    if (requestBody != null) {
+      String json = object2Json(requestBody);
+      log.info("post method is loading... {}", json);
+      return json;
+    }
+    log.info("post method is loading...");
+    return "Greetings from Spring Boot!";
+  }
 
 }
