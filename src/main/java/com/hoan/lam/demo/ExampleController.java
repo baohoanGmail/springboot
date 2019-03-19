ea944e8417b0fc9aac6042265fe391c3c56d662a
baohoan90 <30434634+baohoan90@users.noreply.github.com>
2019-03-19 15:30:41 +0700

    Update ExampleController.java

--- src/main/java/com/hoan/lam/demo/ExampleController.java
+++ src/main/java/com/hoan/lam/demo/ExampleController.java
@@ -1,6 +1,9 @@
 package com.hoan.lam.demo;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +14,10 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -50,10 +57,37 @@ public class ExampleController {
     return "Greetings from Spring Boot!";
   }
 
+  private int deleteMails(Set<String> emailDeleteds) {
+    if (emailDeleteds.isEmpty()) {
+      return -1;
+    }
+    SendGrid sg =
+        new SendGrid("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");
+    DeleteMailRequest deleteMailsRequest = new DeleteMailRequest();
+    deleteMailsRequest.setEmails(emailDeleteds);
+    String requestBody = object2Json(deleteMailsRequest);
+    log.info("REQUEST BODY {}", requestBody);
+    Response response = null;
+    try {
+      Request request = new Request();
+      request.setMethod(Method.DELETE);
+      request.setEndpoint("/suppression/bounces");
+      request.setBody(requestBody);
+      response = sg.api(request);
+      System.out.println(response.getStatusCode());
+      System.out.println(response.getBody());
+      System.out.println(response.getHeaders());
+    } catch (IOException ex) {
+      log.error("Request delete mails failed {}", ex.getMessage());
+    }
+    return (response != null) ? response.getStatusCode() : -1;
+  }
+
   @PostMapping("/load")
   public String load(@RequestBody List<Bounce> requestBody) {
     if (requestBody != null) {
       String json = object2Json(requestBody);
+      deleteMails(requestBody.stream().map(x -> x.getEmail()).collect(Collectors.toSet()));
       log.info("post method is loading... {}", json);
       return json;
     }
