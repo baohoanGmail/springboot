4750216c699bac8fb4d581112a7ff20b8e1efeed
baohoan90 <30434634+baohoan90@users.noreply.github.com>
2019-06-04 14:56:02 +0700

    Update ProductController.java

--- src/main/java/com/hoan/lam/demo/ProductController.java
+++ src/main/java/com/hoan/lam/demo/ProductController.java
@@ -17,7 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.hoan.lam.demo.model.Product;
 import com.hoan.lam.demo.response.ErrorElement;
 import com.hoan.lam.demo.response.RestResponse;
